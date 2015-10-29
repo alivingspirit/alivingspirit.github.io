75bf82444ef7d7bf1cd9197c6227d5c3f04c3c67
Abayai Sherman <alivingspirit@gmail.com>
2015-10-28 21:33:14 -0500

    Added web app

--- ABL Scheduler/main.py
+++ ABL Scheduler/main.py
@@ -1,4 +1,5 @@
 from json import load
+from random import shuffle
 
 def get_teams():
     with open('teams.txt', 'r') as f:
@@ -8,6 +9,9 @@ def get_config():
     with open('config.txt', 'r') as f:
         return load(f)
 
+def get_locations():
+    with open('locations.txt', 'r') as f:
+        return [location.strip() for location in f.readlines()]
 
 if __name__ == '__main__':
     teams = get_teams()
@@ -16,21 +20,24 @@ if __name__ == '__main__':
     num_of_teams = len(teams)
     half_of_teams = num_of_teams / 2
 
+    locations = get_locations()
     weeks = config['Weeks']
-    number_of_games_per_week = config['NumberOfGamesPerWeek']
+    number_of_games_per_week = len(locations)
 
     games = []
 
     result_file = open('result.txt', 'w')
     for week in range(1, weeks + 1):
+        shuffle(locations)
         print("Week " + str(week), file=result_file)
         for game_num in range(number_of_games_per_week):
             game = {
                 'week': week,
                 'team1': teams[game_num],
-                'team2': teams[-(game_num + 1)]
+                'team2': teams[-(game_num + 1)],
+                'locations': locations[game_num]
             }
             games.append(game)
-            print(game['team1'] + ' vs ' + game['team2'], file=result_file)
+            print(game['team1'] + ' vs ' + game['team2'] + ' at ' + locations[game_num], file=result_file)
         print(file=result_file)
         teams = teams[1:] + [teams[0]]
---
+++ ABL Scheduler/webapp.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+import dataset
+from random import shuffle
+from itertools import groupby
+
+app = Flask("ABL")
+app.debug = True
+
+db = dataset.connect('sqlite:///mydatabase.db')
+mydata = db['data']
+schedule_table = db['schedule']
+
+if len(mydata) == 0:
+    mydata.insert({
+        'teams': "",
+        'dates': "",
+        'locations': ""
+    })
+
+def save_data(**kwargs):
+    kwargs['id'] = 1
+    mydata.update(kwargs, ['id'])
+
+def get_data():
+    return mydata.find_one(id=1)
+
+def generate_schedule(teams = '', locations = '', dates = ''):
+    teams = [team.strip() for team in teams.split('\n') ]
+    locations = [location.strip() for location in locations.split('\n')]
+    dates = [date.strip() for date in dates.split('\n')]
+
+    num_of_teams = len(teams)
+    half_of_teams = num_of_teams / 2
+
+    result_file = open('result.txt', 'w')
+    for date in dates:
+        shuffle(locations)
+        for game_num, location in enumerate(locations):
+            yield {
+                'date': date,
+                'team1': teams[game_num],
+                'team2': teams[-(game_num + 1)],
+                'location': location
+            }
+        teams = teams[1:] + [teams[0]]
+
+@app.route('/schedule.html')
+def schedule():
+    schedule = schedule_table.all()
+    schedule = groupby(schedule, lambda s: s['date'] )
+    schedule = [ (date, list(game)) for date, game in schedule ]
+    return render_template('/schedule.html', schedule = schedule )
+
+@app.route('/edit.html')
+def get_edit_page():
+    return render_template('/edit.html', **get_data() )
+
+@app.route('/generateschedule', methods=['post'])
+def generate_shedule():
+    data = dict(
+        teams = request.form['teams'],
+        locations = request.form['locations'],
+        dates = request.form['dates'])
+    save_data(**data)
+
+    schedule_data = generate_schedule(**data)
+    schedule_table.delete()
+    schedule_table.insert_many(list(schedule_data))
+
+    return redirect(url_for('get_edit_page'))
+
+if __name__ == '__main__':
+    app.run()
