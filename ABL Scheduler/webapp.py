0e1257f4e156972c6ee90e536746d3760fb983de
Abayai Sherman <alivingspirit@gmail.com>
2015-11-08 22:40:45 -0600

    edit capability before testing

--- ABL Scheduler/webapp.py
+++ ABL Scheduler/webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import dataset
 from random import shuffle
 from itertools import groupby
+from operator import itemgetter
 
 app = Flask("ABL")
 
@@ -47,8 +48,41 @@ def generate_schedule(teams = '', locations = '', dates = ''):
 def schedule():
     schedule = schedule_table.all()
     schedule = groupby(schedule, lambda s: s['date'] )
-    schedule = [ (date, list(game)) for date, game in schedule ]
+    schedule = [ (date, list(sorted(game, key=itemgetter('location')))) for date, game in schedule ]
     return render_template('/schedule.html', schedule = schedule )
+
+@app.route('/editschedule.html')
+def edit_schedule():
+    schedule = schedule_table.all()
+
+    schedule = groupby(sorted(schedule, key= lambda x: x['date'] ), lambda s: s['date'] )
+    schedule = [ (date, list(sorted(game, key=itemgetter('location')))) for date, game in schedule ]
+    return render_template('/editschedule.html', schedule = schedule )
+
+@app.route('/editscheduleaction', methods=['post'])
+def edit_schedule_entry():
+    id = request.form.get('id', None)
+    date = request.form['date']
+    team1 = request.form['team1']
+    team2 = request.form['team2']
+    location = request.form['location']
+
+     #Actions
+    add = request.form.get('Add', None)
+    update = request.form.get('Update', None)
+    delete = request.form.get('Delete', None)
+
+    if add:
+        schedule_table.insert(dict(date = date, team1 = team1, team2 = team2, location = location), ensure=False)
+
+    if update:
+        schedule_table.update(dict(id = id, date = date, team1 = team1, team2 = team2, location = location), ['id'], ensure=False)
+
+    if delete:
+        schedule_table.delete(id=id)
+
+    return redirect(url_for('edit_schedule'))
+
 
 @app.route('/edit.html')
 def get_edit_page():
@@ -69,4 +103,4 @@ def generate_shedule():
     return redirect(url_for('get_edit_page'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
